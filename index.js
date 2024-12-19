d7f40263eb7cd71af6eb736cc2dc5dff0bbeed56
awale398 <adenabdiawal@gmail.com>
2024-12-19 17:24:51 +0300

    counter program of JS

---
+++ index.js
@@ -0,0 +1,24 @@
+// COUNTER PROGRAM
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("reset");
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLabel = document.getElementById("countlabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
